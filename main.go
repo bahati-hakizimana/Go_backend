c8b78e2b0b96ad8853e196e94d67c9df8b6ce5d7
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-15 09:26:08 -0700

    backend.

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/bahati-hakizimana/Go_backend/database"
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	database.Connect()
+
+	err:=godotenv.Load()
+
+	if  err != nil {
+
+		log.Fatal("erro loading env")
+		
+	}
+
+	port:=os.Getenv("PORT")
+	app:=fiber.New()
+	app.Listen(":" +port)
+
+}
