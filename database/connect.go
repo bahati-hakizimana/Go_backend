15119dad27e717f715b5c373ad5daacc57053f9c
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-15 09:15:57 -0700

    backend

--- database/connect.go
+++ database/connect.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+		
+	}
+	// no error
+
+	dsn:=os.Getenv("DSN")
+	database,err:=gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil{
+		fmt.Println("could not connect to database")
+	}
+	DB=database
+}
